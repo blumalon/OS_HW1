70154dbe7baf3272d0c5897e7df89e55241d8767
User <alon.blum@campus.technion.ac.il>
2025-11-20 14:29:27 +0200

    smash.getPrompt

--- Commands.h
+++ Commands.h
@@ -241,12 +241,15 @@ public:
 
 class SmallShell {
 private:
-    // TODO: Add your data members
+    std::string currentPrompt = "smash";
     SmallShell();
 
 public:
     Command *CreateCommand(const char *cmd_line);
 
+    std::string getPrompt() const {
+        return currentPrompt;
+    }
     SmallShell(SmallShell const &) = delete; // disable copy ctor
     void operator=(SmallShell const &) = delete; // disable = operator
     static SmallShell &getInstance() // make SmallShell singleton
